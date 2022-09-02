556f86982916fa74578583d7d7f447887ff4b94c
Radhakrishna Achanta <rk.epfl@gmail.com>
2022-09-02 12:32:17 +0200

    improved forward transform and added inverse transform

--- demo.py
+++ demo.py
@@ -1,25 +1,32 @@
+'''
+Demo showing forward and inverse Haar wavelet decomposition.
+The input can be in color or grayscale.
+
+Please note that in order to save as an image, a clipping operation
+is performed on the Haar wavelets, which discards negative values.
+'''
+
 import numpy as np
 from timeit import default_timer as timer
 from PIL import Image
-from haar_wavelets import haar_decomposition
+from haar_wavelets import haar_forward, haar_inverse
 
 
 
 def main():
+
     ref = Image.open("./bee.png")
-    ref = np.asarray(ref)
-    print(ref.shape)
+    ref = np.asarray(ref).astype(np.float32)
 
+    LEVELS = 2
     start = timer()
-    out = (haar_decomposition(ref[:,:,0])).astype(np.int64)
-
-    # out = 255*(out-out.min())/(out.max()-out.min())
+    hwt = haar_forward(ref.copy(), LEVELS) # forward transform
+    inv = haar_inverse(hwt.copy(), LEVELS)  # backward transform
     end = timer()
 
-    out = out.clip(0,255).astype(np.uint8)
-    # Image.fromarray(ref[:,:,0]).save("orig.png")
-    Image.fromarray(out).save("bee_wavelets.png")
-    print("time taken in seconds = ", end-start)
+    Image.fromarray(hwt.clip(0,255).astype(np.uint8)).save("bee_wavelets_for.png")
+    Image.fromarray(inv.clip(0,255).astype(np.uint8)).save("bee_wavelets_inv.png")
+    print("Time taken in seconds for forward and inverse Haar wavelet transform:", end-start)
 
     
 if __name__ == "__main__":
--- haar_wavelets.py
+++ haar_wavelets.py
@@ -1,39 +1,44 @@
 '''
-The function haar_decomposition takes a 2D color or grayscale images and
-returns a 2D array containing multi-resolution haar wavelet coefficients.
-The number of levels is a user-provided parameter.
+The function haar_forward takes a 2D color or grayscale image and
+returns an array of the same shape as the input image containing
+multi-resolution haar wavelet coefficients.The number of levels
+of decomposition is a user-provided parameter.
+
+The function haar_inverse takes multi-resolution haar wavelets and
+the levels of decomposition as inputs and reconstructs the original
+input. The number of levels provided should be the same as the for
+the decomposition.
+
+Please note that if the X (or Y) dimension is of odd length, the
+last column (or row) is not processed during the decomposition.
 '''
 
 import numpy as np
 
-def haar_decomposition(image,levels=3):
+def haar_forward(image,levels=3):
 
 
     def one_level_decomp(img):
 
-        H,W = img.shape[0],img.shape[1]
-        hwt = np.zeros(img.shape,dtype=np.int64)
+        Y,X = img.shape[0],img.shape[1]
+        Y,X = (Y//2)*2, (X//2)*2
 
-        Y,X = H//2,W//2
+        a = img[:,0:X:2,...].copy()
+        b = img[:,1:X:2,...].copy()
 
-        ax = img[:,0:X*2:2,...]
-        bx = img[:,1:X*2:2,...]
+        img[:, :X//2,...] = (a + b)/2
+        img[:,X//2:X,...] = (a - b)/2
 
-        # hwt[:,:X,...] = (ax+bx)
-        hwt[:,:X,...] = np.right_shift(ax+bx,1)
-        hwt[:,X:X*2,...] = ax-bx
+        a = img[0:Y:2,:,...].copy()
+        b = img[1:Y:2,:,...].copy()
 
-        ay = hwt[0:Y*2:2,:,...]
-        by = hwt[1:Y*2:2,:,...]
-
-        # img[:Y,:,...] = (ay+by)
-        img[:Y,:,...] = np.right_shift(ay+by,1)
-        img[Y:Y*2,:,...] = ay-by
+        img[:Y//2, :,...] = (a + b)/2
+        img[Y//2:Y,:,...] = (a - b)/2
 
         return img
 
-    def multi_scale_decomp(image,levels):
-        img = image.copy()
+    def multi_scale_decomp(img,levels):
+
         H,W = img.shape[0],img.shape[1]
 
         for s in range(levels):
@@ -42,3 +47,41 @@ def haar_decomposition(image,levels=3):
         return img
 
     return multi_scale_decomp(image,levels)
+
+
+def haar_inverse(image,levels=3):
+
+
+    def one_level_recomp(img):
+
+        Y,X = img.shape[0],img.shape[1]
+        Y,X = (Y//2)*2, (X//2)*2
+
+        a = img[:, :X//2,...].copy()
+        b = img[:,X//2:X,...].copy()
+
+        img[:,0:X:2,...] = a + b
+        img[:,1:X:2,...] = a - b
+
+
+        a = img[:Y//2, :,...].copy()
+        b = img[Y//2:Y,:,...].copy()
+
+        img[0:Y:2,:,...] = a + b
+        img[1:Y:2,:,...] = a - b
+
+        return img
+
+    def multi_scale_recomp(img,levels):
+
+        H,W = img.shape[0],img.shape[1]
+
+        for s in reversed(range(levels)):
+            img[:(H>>s),:(W>>s),...] = one_level_recomp(img[:(H>>s),:(W>>s),...])
+
+        return img
+
+    return multi_scale_recomp(image,levels)
+
+
+
